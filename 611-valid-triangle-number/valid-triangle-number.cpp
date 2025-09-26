4dd63f241843b12fbc61ee56be0254a03686d833
Kailash Chandra Sharma <142252237+Kailash-03@users.noreply.github.com>
2025-09-27 01:29:23 +0530

    Time: 27 ms (89.63%) | Memory: 16.7 MB (8.84%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int ans=0,n=nums.size();
+        sort(nums.begin(),nums.end());
+
+        for(int k=n-1;k>=2;k--)
+        {
+            int i=0,j=k-1;
+            while(i<j)
+            {
+                if(nums[i]+nums[j]>nums[k])
+                {
+                    ans+=j-i;
+                    j--;
+                }
+                else
+                i++;
+            }
+        }
+        return ans;
+    }
+};
